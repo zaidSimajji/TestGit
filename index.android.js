9335fc7eb2955487a7194e9a6278622dc09ad12c
zaidSimajji <zaid@binary-vintage.com>
2017-07-27 18:37:43 +0800

    Edit android entry
    Edit the Android Entry Page

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class TestGit extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native Android VErsion!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
